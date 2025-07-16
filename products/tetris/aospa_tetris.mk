98cfd391d577b28661233ed40fe955e5f365e103
SuperAviation001 <superaviation001@gmail.com>
2025-07-17 00:26:02 +1000

    aospa: products: Welcome to @AOSPA (or @AOSPA-tetris), tetris.

---
+++ products/tetris/aospa_tetris.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+# Inherit from tetris device
+$(call inherit-product, device/nothing/tetris/device.mk)
+
+PRODUCT_NAME := aospa_tetris
+PRODUCT_DEVICE := tetris
+PRODUCT_BRAND := Nothing
+PRODUCT_MANUFACTURER := Nothing
+PRODUCT_MODEL := A015
+
+PRODUCT_GMS_CLIENTID_BASE := android-nothing
+
+DEVICE_CODENAME := tetris
+
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="Tetris 15 AP3A.240905.015 2502082015 release-keys" \
+    BuildFingerprint=Nothing/Tetris/Tetris:15/AP3A.240905.015.A2/2502082015:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
